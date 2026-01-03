3a585e9e972da8235dbc275c939d8971750eacd2
Thanniru-yaswanth03 <your-email@gmail.com>
2026-01-03 11:46:02 +0530

    Day 1 part 1  and part 2 done pretty basics so i didnt write any code

---
+++ JavaScript/Basics.js
@@ -0,0 +1,55 @@
+// Operator Precedence
+// ()
+// **
+// *,/,%
+// +,-
+// (5+2)/7+1*2=?
+// 3/1+2**2
+// var result1= 3
+// var result2= 7
+// let age1 =21;
+// let age2 =22;
+// const pi=3.14;
+// // in pre change and use 
+// // in post use and change
+
+// // age+=1;
+// console.log("1 is "+age1);
+// console.log("2 is "+age2);
+// // console.log(pi);
+// console.log(age1++);
+// console.log(++age1);
+// console.log(age2--);
+// console.log(--age2);
+// let x =5;
+// let y = x++ + ++x;
+// console.log(y);
+// == only val and === both type and val
+// let p =12;
+// let e =11;
+// let o = "The cost is "+(p+e)+"rupees";
+// console.log(o);
+// console.log(`Hey there the price is ${o} rupees`);
+// good str q
+// let str1 = 'asking';
+// let str2 = 'ask';
+// if((str1.length>3) && (str1[0]==='a')){
+//     console.log(`${str1} is a good string`)
+// }else{
+//     console.log("nah");
+// }
+// let key ='yash';
+// switch (key) {
+//     case 'yash':
+//         console.log(`yeah his name is ${key}`)
+//         break;
+//     case 'batman':
+//         console.log("nah");
+//         break;
+
+//     default:
+//         break;
+// }
+let name = prompt(" ?  : ");
+alert("wassup ? ")
+console.log(name);
